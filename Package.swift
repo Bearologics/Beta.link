6a33e0215867eea5463c5c9491825e36c767f8e6
Marcus Kida <marcus.kida@bearologics.com>
2020-08-18 19:12:56 +0200

    Upgrade to Publish 0.7.0

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .executable(name: "BetaLink", targets: ["BetaLink"])
     ],
     dependencies: [
-        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.5.0"),
+        .package(name: "Publish", url: "https://github.com/johnsundell/publish.git", from: "0.7.0"),
         .package(name: "SplashPublishPlugin", url: "https://github.com/kimar/SplashPublishPlugin.git", from: "0.2.0")
     ],
     targets: [
